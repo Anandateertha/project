4b2c7c4e93b44b55adbf81af58939a70ac61dca2
Anandateertha <www.anandateertha@gmail.com>
2023-12-25 18:31:29 +0530

    fc

---
+++ src/main/java/com/signup/project/ProjectController.java
@@ -0,0 +1,20 @@
+package com.signup.project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class ProjectController {
+
+    @Autowired
+    public ProjectService projectService;
+    
+    @GetMapping("test")
+    public String user()
+    {
+        return projectService.addService();
+    }
+}
---
+++ src/main/java/com/signup/project/ProjectService.java
@@ -0,0 +1,11 @@
+package com.signup.project;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+    public String addService()
+    {
+        return "User package ";
+    }
+}
